a5baed86926b6556db78dfdb20180951314becbb
1988db <elozrana@gmail.com>
2022-06-03 13:33:10 +0200

    fix: slide-in animation change

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,8 +1,33 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const burger = document.getElementById('burger');
-    const menu = document.getElementById('mobile-menu');    
-    burger.addEventListener('click', openCloseMenu);    
-    function openCloseMenu () {
-        menu.classList.toggle('visible');
+    const logoWrapper = document.getElementById('logo-wrapper');
+    const mainNav = document.getElementById('main-nav')
+    const mainMenu = document.getElementById('main-menu');
+    let isMenuOpen = false;
+    burger.addEventListener('click', showHideMenu);
+
+    function showHideMenu () {
+        if(isMenuOpen) {
+            logoWrapper.classList.toggle('visible');
+            burger.classList.toggle('visible');
+            mainNav.classList.toggle('visible');
+            setTimeout(()=> {
+                mainMenu.style.display = 'none';
+                mainNav.style.display = 'none';
+            }, 510);
+            isMenuOpen = false;
+            
+        } else {
+            logoWrapper.classList.toggle('visible');
+            burger.classList.toggle('visible');
+            mainMenu.style.display = 'flex';
+            mainNav.style.display = 'flex';
+            mainNav.classList.toggle('visible');
+            isMenuOpen = true;       
+        }
+        
+        
+        
     }
+    
 });
